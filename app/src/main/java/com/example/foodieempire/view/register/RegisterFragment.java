e39bdfdf122bff7e6bfd106118064be705a14a17
Heba-Alii <hebaaliali18@gmail.com>
2023-03-27 20:09:13 +0200

    register user data with firebase Auth

--- app/src/main/java/com/example/foodieempire/view/register/RegisterFragment.java
+++ app/src/main/java/com/example/foodieempire/view/register/RegisterFragment.java
@@ -6,19 +6,26 @@ import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.foodieempire.R;
 import com.example.foodieempire.databinding.FragmentRegisterBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.Objects;
 
 public class RegisterFragment extends Fragment {
     FragmentRegisterBinding binding;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,6 +39,23 @@ public class RegisterFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.createUserWithEmailAndPassword(Objects.requireNonNull(binding.mailET.getEditText()).getText().toString(), Objects.requireNonNull(binding.passET.getEditText()).getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Navigation.findNavController(binding.getRoot()).navigate(R.id.action_registerFragment_to_loginFragment);
+                                } else {
+                                    Toast.makeText(getActivity(), task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+
+                            }
+                        });
+            }
+        });
 
 
     }
