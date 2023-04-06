62abcd868138de74c103cf1f27e5b2554ad4bb7d
Heba-Alii <hebaaliali18@gmail.com>
2023-04-06 04:30:48 +0200

    profile design and logout button action to logout from firebase

--- app/src/main/java/com/example/foodieempire/view/main/ProfileFragment.java
+++ app/src/main/java/com/example/foodieempire/view/main/ProfileFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +13,13 @@ import android.view.ViewGroup;
 
 import com.example.foodieempire.R;
 import com.example.foodieempire.databinding.FragmentProfileBinding;
+import com.example.foodieempire.view.register.RegistrationActivity;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileFragment extends Fragment {
 
     FragmentProfileBinding binding;
+    FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,5 +32,14 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        binding.logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                firebaseAuth.signOut();
+                Navigation.findNavController(binding.getRoot()).navigate(R.id.action_profileFragment_to_registrationActivity);
+                getActivity().finish();
+            }
+        });
+
     }
 }
